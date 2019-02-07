2caaa3d0596174149992a8abe839540f81fcba56
DD-HAREKAZE <zhao.jiawe@northeastern.edu>
2019-02-07 12:29:43 -0500

    delete useless line

--- infrastructure/aws/cloudformation/csye6225-aws-networking-setup.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-networking-setup.sh
@@ -3,7 +3,6 @@ subnet1=$(aws ec2 create-subnet --vpc-id $vpcinfo --cidr-block 10.0.1.0/24 |jq -
 subnet2=$(aws ec2 create-subnet --vpc-id $vpcinfo --cidr-block 10.0.2.0/24 |jq -r ".Subnet.SubnetId")
 subnet3=$(aws ec2 create-subnet --vpc-id $vpcinfo --cidr-block 10.0.3.0/24 |jq -r ".Subnet.SubnetId")
 gateway=$(aws ec2 create-internet-gateway | jq -r ".InternetGateway.InternetGatewayId")
-echo $temp |jq '.'
 aws ec2 attach-internet-gateway --internet-gateway-id $gateway --vpc-id $vpcinfo
 routetable=$(aws ec2 create-route-table --vpc-id $vpcinfo | jq -r ".RouteTable.RouteTableId")
 aws ec2 associate-route-table  --subnet-id $subnet1 --route-table-id $routetable
