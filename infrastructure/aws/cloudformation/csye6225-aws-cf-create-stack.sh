180f53ea919ae4fcafc87628035290ecb4651db8
DD-HAREKAZE <zhao.jiawe@northeastern.edu>
2019-02-07 15:03:12 -0500

    finish four scriptfiles but delete needs to improve

---
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-stack.sh
@@ -0,0 +1,1 @@
+aws cloudformation create-stack --stack-name csye6225-vpc --template-body file://create.yaml
---
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-terminate-stack.sh
@@ -0,0 +1,2 @@
+ aws cloudformation delete-stack --stack-name csye6225-VPC
+#change the stack-name to choose which one to delete
