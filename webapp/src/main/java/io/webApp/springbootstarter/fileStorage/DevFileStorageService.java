80735c7a2314af7bd6d05500d4c74eb6adf25c51
amrithprabhu <prabhu.am@husky.neu.edu>
2019-03-16 19:39:48 -0700

    change

--- webapp/src/main/java/io/webApp/springbootstarter/fileStorage/DevFileStorageService.java
+++ webapp/src/main/java/io/webApp/springbootstarter/fileStorage/DevFileStorageService.java
@@ -39,4 +39,75 @@ public class DevFileStorageService implements FileStorageService {
 	private String endpointUrl;
 	@Value("${bucketName}")
 	private String bucketName;
-	
+
+
+	@PostConstruct
+	private void initializeAmazon() {
+
+		this.s3client = AmazonS3ClientBuilder.standard().withCredentials(new InstanceProfileCredentialsProvider(false)).build();
+	}
+
+	private File convertMultiPartToFile(MultipartFile file) throws IOException {
+		File convFile = new File(file.getOriginalFilename());
+		FileOutputStream fos = new FileOutputStream(convFile);
+		fos.write(file.getBytes());
+		fos.close();
+		return convFile;
+
+	}
+
+	private String generateFileName(MultipartFile multiPart) {
+		return multiPart.getOriginalFilename();
+	}
+
+
+
+	public String storeFile(MultipartFile multipartFile) {
+		String fileUrl = "";
+		try {
+			File file = convertMultiPartToFile(multipartFile);
+			String fileName = generateFileName(multipartFile);
+			fileStorageLocation = Paths.get(endpointUrl + bucketName);
+			fileUrl = fileStorageLocation + fileName;
+			uploadFileTos3bucket(fileName, file);
+			file.delete();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return fileUrl;
+	}
+
+
+	public Path getFileStorageLocation() {
+		return fileStorageLocation;
+	}
+
+
+	public boolean DeleteFile(String fileUrl) {
+		 try {
+		        String fileName = fileUrl.substring(fileUrl.lastIndexOf("/") + 1);
+		        System.out.println("fileName : " + fileName);
+		        s3client.deleteObject(new DeleteObjectRequest(bucketName, fileName));
+		        }
+		        catch (AmazonServiceException e) {
+		      
+		            e.printStackTrace();
+		            return false;
+		            } catch (SdkClientException e) {
+		 
+		            e.printStackTrace();
+		            return false;
+		            }
+		        return true;
+	}
+
+	private void uploadFileTos3bucket(String fileName, File file) {
+		try {
+		s3client.putObject(new PutObjectRequest(bucketName, fileName, file));
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
